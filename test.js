dd58aee256b04d954c98ea1773b5e60747abc6fc
Seph Gentle <me@josephg.com>
2020-05-16 11:19:40 +1000

    Added testcase for #1

--- test.js
+++ test.js
@@ -6,3 +6,11 @@ assert.strictEqual(strPosToUni('💃123'), 4)
 
 assert.strictEqual(uniToStrPos('💃123', 1), 2)
 assert.strictEqual(uniToStrPos('💃123', 4), 5)
+
+
+// Characters in the U+E000 - U+FFFF are treated as a single character
+assert.strictEqual(strPosToUni('ﬀ123', 1), 1)
+assert.strictEqual(strPosToUni('ﬀ123'), 4)
+
+assert.strictEqual(uniToStrPos('ﬀ123', 1), 1)
+assert.strictEqual(uniToStrPos('ﬀ123', 4), 4)
